f7dce46e0acf40ddb0d977ab19e770810c033809
nagynoel79 <kerekesbalint11@gmail.com>
2025-12-04 18:00:10 +0100

    Create MainForm for chat interface

---
+++ ChatClientGUI/Forms/MainForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ChatClientGUI.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lstMessages = new ListBox();
+            txtMessage = new TextBox();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // lstMessages
+            // 
+            lstMessages.FormattingEnabled = true;
+            lstMessages.ItemHeight = 15;
+            lstMessages.Location = new Point(110, 47);
+            lstMessages.Name = "lstMessages";
+            lstMessages.Size = new Size(246, 139);
+            lstMessages.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(110, 203);
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(100, 23);
+            txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            btnSend.Location = new Point(232, 202);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(75, 23);
+            btnSend.TabIndex = 2;
+            btnSend.Text = "button1";
+            btnSend.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSend);
+            Controls.Add(txtMessage);
+            Controls.Add(lstMessages);
+            Name = "MainForm";
+            Text = "MainForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lstMessages;
+        private TextBox txtMessage;
+        private Button btnSend;
+    }
+}
---
+++ ChatClientGUI/Forms/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChatClientGUI.Services;
+
+namespace ChatClientGUI.Forms
+{
+    public partial class MainForm : Form
+    {
+        private ClientService _service;
+
+        public MainForm(ClientService service)
+        {
+            InitializeComponent();
+            _service = service;
+            _service.MessageReceived += OnMessageReceived;
+            _service.ConnectionLost += OnConnectionLost;
+            btnSend.Click += async (s, e) => await SendMessage();
+        }
+
+        private async Task SendMessage()
+        {
+            if (string.IsNullOrWhiteSpace(txtMessage.Text)) return;
+            await _service.SendMessageAsync(txtMessage.Text);
+            txtMessage.Clear();
+        }
+
+        private void OnMessageReceived(string msg)
+        {
+            if (IsDisposed) return;
+            Invoke((MethodInvoker)delegate
+            {
+                lstMessages.Items.Add(msg);
+                lstMessages.TopIndex = lstMessages.Items.Count - 1;
+            });
+        }
+
+        private void OnConnectionLost()
+        {
+            if (IsDisposed) return;
+            Invoke((MethodInvoker)delegate
+            {
+                MessageBox.Show("Connection lost from server.");
+                Application.Exit();
+            });
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
