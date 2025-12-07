10cf973f8b402185772e9af90f5e9d3bb40e5441
balintkrks <kerekesbalint11@gmail.com>
2025-12-07 22:26:03 +0100

    Initialize ChatAI project and add ChatCommon reference

---
+++ ChatCommon/Utils/HashHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ChatCommon.Utils
+{
+    public static class HashHelper
+    {
+        public static string Sha256(string input)
+        {
+            using var sha = SHA256.Create();
+            var bytes = Encoding.UTF8.GetBytes(input);
+            var hash = sha.ComputeHash(bytes);
+            return Convert.ToHexString(hash);
+        }
+    }
+}
