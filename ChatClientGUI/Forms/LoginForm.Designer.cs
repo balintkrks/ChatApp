caa42958810ac895c21088bc4ba42026ff97b40e
nagynoel79 <nagynoel79@gmail.com>
2025-12-08 16:21:25 +0100

    Modernize Login Form to match application style

--- ChatClientGUI/Forms/LoginForm.Designer.cs
+++ ChatClientGUI/Forms/LoginForm.Designer.cs
@@ -13,130 +13,178 @@
 			base.Dispose(disposing);
 		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        private void InitializeComponent()
-        {
-            txtUsername = new TextBox();
-            txtPassword = new TextBox();
-            btnLogin = new Button();
-            btnRegister = new Button();
-            lblUser = new Label();
-            lblPass = new Label();
-            pnlMain = new Panel();
-            pnlMain.SuspendLayout();
-            SuspendLayout();
-            // 
-            // txtUsername
-            // 
-            txtUsername.Font = new Font("Segoe UI", 10F);
-            txtUsername.Location = new Point(35, 45);
-            txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(230, 25);
-            txtUsername.TabIndex = 0;
-            // 
-            // txtPassword
-            // 
-            txtPassword.Font = new Font("Segoe UI", 10F);
-            txtPassword.Location = new Point(35, 105);
-            txtPassword.Name = "txtPassword";
-            txtPassword.PasswordChar = '•';
-            txtPassword.Size = new Size(230, 25);
-            txtPassword.TabIndex = 1;
-            // 
-            // btnLogin
-            // 
-            btnLogin.BackColor = Color.DodgerBlue;
-            btnLogin.Cursor = Cursors.Hand;
-            btnLogin.FlatAppearance.BorderSize = 0;
-            btnLogin.FlatStyle = FlatStyle.Flat;
-            btnLogin.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            btnLogin.ForeColor = Color.White;
-            btnLogin.Location = new Point(35, 150);
-            btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(110, 35);
-            btnLogin.TabIndex = 2;
-            btnLogin.Text = "Login";
-            btnLogin.UseVisualStyleBackColor = false;
-            btnLogin.Click += btnLogin_Click;
-            // 
-            // btnRegister
-            // 
-            btnRegister.BackColor = Color.ForestGreen;
-            btnRegister.Cursor = Cursors.Hand;
-            btnRegister.FlatAppearance.BorderSize = 0;
-            btnRegister.FlatStyle = FlatStyle.Flat;
-            btnRegister.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            btnRegister.ForeColor = Color.White;
-            btnRegister.Location = new Point(155, 150);
-            btnRegister.Name = "btnRegister";
-            btnRegister.Size = new Size(110, 35);
-            btnRegister.TabIndex = 3;
-            btnRegister.Text = "Register";
-            btnRegister.UseVisualStyleBackColor = false;
-            // 
-            // lblUser
-            // 
-            lblUser.AutoSize = true;
-            lblUser.Font = new Font("Segoe UI", 9F);
-            lblUser.ForeColor = Color.Gray;
-            lblUser.Location = new Point(35, 27);
-            lblUser.Name = "lblUser";
-            lblUser.Size = new Size(60, 15);
-            lblUser.TabIndex = 4;
-            lblUser.Text = "Username";
-            // 
-            // lblPass
-            // 
-            lblPass.AutoSize = true;
-            lblPass.Font = new Font("Segoe UI", 9F);
-            lblPass.ForeColor = Color.Gray;
-            lblPass.Location = new Point(35, 87);
-            lblPass.Name = "lblPass";
-            lblPass.Size = new Size(57, 15);
-            lblPass.TabIndex = 5;
-            lblPass.Text = "Password";
-            // 
-            // pnlMain
-            // 
-            pnlMain.BackColor = Color.White;
-            pnlMain.Controls.Add(lblUser);
-            pnlMain.Controls.Add(btnRegister);
-            pnlMain.Controls.Add(txtUsername);
-            pnlMain.Controls.Add(btnLogin);
-            pnlMain.Controls.Add(lblPass);
-            pnlMain.Controls.Add(txtPassword);
-            pnlMain.Location = new Point(48, 41);
-            pnlMain.Name = "pnlMain";
-            pnlMain.Size = new Size(300, 220);
-            pnlMain.TabIndex = 6;
-            // 
-            // LoginForm
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.WhiteSmoke;
-            ClientSize = new Size(399, 311);
-            Controls.Add(pnlMain);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
-            MaximizeBox = false;
-            Name = "LoginForm";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "ChatApp - Login";
-            pnlMain.ResumeLayout(false);
-            pnlMain.PerformLayout();
-            ResumeLayout(false);
+		private void InitializeComponent()
+		{
+			this.txtUsername = new System.Windows.Forms.TextBox();
+			this.txtPassword = new System.Windows.Forms.TextBox();
+			this.btnLogin = new System.Windows.Forms.Button();
+			this.btnRegister = new System.Windows.Forms.Button();
+			this.lblUser = new System.Windows.Forms.Label();
+			this.lblPass = new System.Windows.Forms.Label();
+			this.pnlHeader = new System.Windows.Forms.Panel();
+			this.btnMinimize = new System.Windows.Forms.Button();
+			this.btnClose = new System.Windows.Forms.Button();
+			this.lblTitle = new System.Windows.Forms.Label();
+			this.pnlHeader.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// txtUsername
+			// 
+			this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 11F);
+			this.txtUsername.Location = new System.Drawing.Point(50, 80);
+			this.txtUsername.Name = "txtUsername";
+			this.txtUsername.Size = new System.Drawing.Size(250, 27);
+			this.txtUsername.TabIndex = 0;
+			// 
+			// txtPassword
+			// 
+			this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 11F);
+			this.txtPassword.Location = new System.Drawing.Point(50, 150);
+			this.txtPassword.Name = "txtPassword";
+			this.txtPassword.PasswordChar = '•';
+			this.txtPassword.Size = new System.Drawing.Size(250, 27);
+			this.txtPassword.TabIndex = 1;
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.BackColor = System.Drawing.Color.DodgerBlue;
+			this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnLogin.FlatAppearance.BorderSize = 0;
+			this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+			this.btnLogin.ForeColor = System.Drawing.Color.White;
+			this.btnLogin.Location = new System.Drawing.Point(50, 210);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(115, 40);
+			this.btnLogin.TabIndex = 2;
+			this.btnLogin.Text = "Login";
+			this.btnLogin.UseVisualStyleBackColor = false;
+			// 
+			// btnRegister
+			// 
+			this.btnRegister.BackColor = System.Drawing.Color.Gray;
+			this.btnRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnRegister.FlatAppearance.BorderSize = 0;
+			this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+			this.btnRegister.ForeColor = System.Drawing.Color.White;
+			this.btnRegister.Location = new System.Drawing.Point(185, 210);
+			this.btnRegister.Name = "btnRegister";
+			this.btnRegister.Size = new System.Drawing.Size(115, 40);
+			this.btnRegister.TabIndex = 3;
+			this.btnRegister.Text = "Register";
+			this.btnRegister.UseVisualStyleBackColor = false;
+			// 
+			// lblUser
+			// 
+			this.lblUser.AutoSize = true;
+			this.lblUser.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+			this.lblUser.ForeColor = System.Drawing.Color.Gray;
+			this.lblUser.Location = new System.Drawing.Point(50, 60);
+			this.lblUser.Name = "lblUser";
+			this.lblUser.Size = new System.Drawing.Size(64, 15);
+			this.lblUser.TabIndex = 4;
+			this.lblUser.Text = "Username";
+			// 
+			// lblPass
+			// 
+			this.lblPass.AutoSize = true;
+			this.lblPass.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+			this.lblPass.ForeColor = System.Drawing.Color.Gray;
+			this.lblPass.Location = new System.Drawing.Point(50, 130);
+			this.lblPass.Name = "lblPass";
+			this.lblPass.Size = new System.Drawing.Size(59, 15);
+			this.lblPass.TabIndex = 5;
+			this.lblPass.Text = "Password";
+			// 
+			// pnlHeader
+			// 
+			this.pnlHeader.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.pnlHeader.Controls.Add(this.btnMinimize);
+			this.pnlHeader.Controls.Add(this.btnClose);
+			this.pnlHeader.Controls.Add(this.lblTitle);
+			this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+			this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+			this.pnlHeader.Name = "pnlHeader";
+			this.pnlHeader.Size = new System.Drawing.Size(350, 40);
+			this.pnlHeader.TabIndex = 6;
+			// 
+			// btnMinimize
+			// 
+			this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnMinimize.FlatAppearance.BorderSize = 0;
+			this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnMinimize.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+			this.btnMinimize.Location = new System.Drawing.Point(270, 0);
+			this.btnMinimize.Name = "btnMinimize";
+			this.btnMinimize.Size = new System.Drawing.Size(40, 40);
+			this.btnMinimize.TabIndex = 2;
+			this.btnMinimize.Text = "−";
+			this.btnMinimize.UseVisualStyleBackColor = true;
+			// 
+			// btnClose
+			// 
+			this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnClose.FlatAppearance.BorderSize = 0;
+			this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed;
+			this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnClose.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.btnClose.Location = new System.Drawing.Point(310, 0);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(40, 40);
+			this.btnClose.TabIndex = 1;
+			this.btnClose.Text = "✕";
+			this.btnClose.UseVisualStyleBackColor = true;
+			// 
+			// lblTitle
+			// 
+			this.lblTitle.AutoSize = true;
+			this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+			this.lblTitle.ForeColor = System.Drawing.Color.DimGray;
+			this.lblTitle.Location = new System.Drawing.Point(12, 10);
+			this.lblTitle.Name = "lblTitle";
+			this.lblTitle.Size = new System.Drawing.Size(46, 19);
+			this.lblTitle.TabIndex = 0;
+			this.lblTitle.Text = "Login";
+			// 
+			// LoginForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(350, 300);
+			this.Controls.Add(this.pnlHeader);
+			this.Controls.Add(this.lblPass);
+			this.Controls.Add(this.lblUser);
+			this.Controls.Add(this.btnRegister);
+			this.Controls.Add(this.btnLogin);
+			this.Controls.Add(this.txtPassword);
+			this.Controls.Add(this.txtUsername);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "LoginForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ChatApp - Login";
+			this.pnlHeader.ResumeLayout(false);
+			this.pnlHeader.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
 
-        }
+		#endregion
 
-        #endregion
-
-        private System.Windows.Forms.TextBox txtUsername;
+		private System.Windows.Forms.TextBox txtUsername;
 		private System.Windows.Forms.TextBox txtPassword;
 		private System.Windows.Forms.Button btnLogin;
 		private System.Windows.Forms.Button btnRegister;
 		private System.Windows.Forms.Label lblUser;
 		private System.Windows.Forms.Label lblPass;
-		private System.Windows.Forms.Panel pnlMain;
+		private System.Windows.Forms.Panel pnlHeader;
+		private System.Windows.Forms.Label lblTitle;
+		private System.Windows.Forms.Button btnClose;
+		private System.Windows.Forms.Button btnMinimize;
 	}
 }
--- ChatClientGUI/Forms/LoginForm.cs
+++ ChatClientGUI/Forms/LoginForm.cs
@@ -1,100 +1,195 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ChatClientGUI.Services;
 
 namespace ChatClientGUI.Forms
 {
-    public partial class LoginForm : Form
-    {
-        private readonly ClientService _service;
+	public partial class LoginForm : Form
+	{
+		// --- ABLAK MOZGATÁS (WinAPI) ---
+		[DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
+		private extern static void ReleaseCapture();
+		[DllImport("user32.dll", EntryPoint = "SendMessage")]
+		private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
 
-        public LoginForm()
-        {
-            InitializeComponent();
-            _service = new ClientService();
-            _service.MessageReceived += OnMessageReceived;
+		private readonly ClientService _service;
 
-            btnLogin.Click += async (s, e) => await HandleLogin();
-            btnRegister.Click += async (s, e) => await HandleRegister();
-        }
+		public LoginForm()
+		{
+			InitializeComponent();
+			_service = new ClientService();
+			_service.MessageReceived += OnMessageReceived;
 
-        private async Task<bool> ConnectIfNeeded()
-        {
-            return await _service.ConnectAsync("127.0.0.1", 5000);
-        }
+			// Ablak stílus
+			this.FormBorderStyle = FormBorderStyle.None;
+			this.DoubleBuffered = true;
 
-        private async Task HandleLogin()
-        {
-            if (!await ConnectIfNeeded())
-            {
-                MessageBox.Show("Could not connect to server.", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
+			// Gombok kerekítése
+			ApplyRoundedRegion(btnLogin, 20);
+			ApplyRoundedRegion(btnRegister, 20);
 
-            string user = txtUsername.Text.Trim();
-            string pass = txtPassword.Text.Trim();
+			// Input mezők stílusa (keret eltüntetése, hogy mi rajzolhassuk)
+			txtUsername.BorderStyle = BorderStyle.None;
+			txtUsername.BackColor = Color.FromArgb(240, 242, 245);
+			txtPassword.BorderStyle = BorderStyle.None;
+			txtPassword.BackColor = Color.FromArgb(240, 242, 245);
 
-            if (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(pass))
-            {
-                await _service.SendLoginAnonAsync();
-            }
-            else
-            {
-                await _service.SendLoginAsync(user, pass);
-            }
-        }
+			// Események
+			btnLogin.Click += async (s, e) => await HandleLogin();
+			btnRegister.Click += async (s, e) => await HandleRegister();
+			btnClose.Click += (s, e) => Application.Exit();
+			btnMinimize.Click += (s, e) => this.WindowState = FormWindowState.Minimized;
 
-        private async Task HandleRegister()
-        {
-            if (!await ConnectIfNeeded())
-            {
-                MessageBox.Show("Could not connect to server.", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
+			// Mozgatás
+			pnlHeader.MouseDown += Header_MouseDown;
+			lblTitle.MouseDown += Header_MouseDown;
 
-            string user = txtUsername.Text.Trim();
-            string pass = txtPassword.Text.Trim();
+			// Input mezők hátterének rajzolása
+			this.Paint += LoginForm_Paint;
 
-            if (!string.IsNullOrEmpty(user) && !string.IsNullOrEmpty(pass))
-            {
-                await _service.SendRegisterAsync(user, pass);
-            }
-            else
-            {
-                MessageBox.Show("Username and password required.", "Registration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
+			// Enter a jelszónál
+			txtPassword.KeyDown += async (s, e) =>
+			{
+				if (e.KeyCode == Keys.Enter)
+				{
+					e.SuppressKeyPress = true;
+					await HandleLogin();
+				}
+			};
+		}
 
-        private void OnMessageReceived(string msg)
-        {
-            if (!IsHandleCreated) return;
+		// --- RAJZOLÁS ---
+		private void LoginForm_Paint(object sender, PaintEventArgs e)
+		{
+			e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
 
-            Invoke((MethodInvoker)delegate
-            {
-                if (msg.Contains("Login successful"))
-                {
-                    string username = msg.Contains("(Anon)") ? "Anon" : txtUsername.Text;
-                    if (string.IsNullOrEmpty(username)) username = "Anon";
+			// Vékony keret az ablak köré
+			using (var borderPen = new Pen(Color.LightGray))
+			{
+				e.Graphics.DrawRectangle(borderPen, 0, 0, this.Width - 1, this.Height - 1);
+			}
 
-                    var mainForm = new MainForm(_service, username);
-                    mainForm.Show();
-                    this.Hide();
-                }
-                else if (msg.Contains("Registration successful"))
-                {
-                    MessageBox.Show("Registration successful! You can now login.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else if (msg.Contains("failed"))
-                {
-                    MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            });
-        }
+			// Username mező háttere
+			Rectangle rectUser = new Rectangle(txtUsername.Location.X - 10, txtUsername.Location.Y - 5, txtUsername.Width + 20, txtUsername.Height + 10);
+			using (GraphicsPath path = GetRoundedPath(rectUser, 15))
+			using (var brush = new SolidBrush(Color.FromArgb(240, 242, 245)))
+			{
+				e.Graphics.FillPath(brush, path);
+			}
 
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
+			// Password mező háttere
+			Rectangle rectPass = new Rectangle(txtPassword.Location.X - 10, txtPassword.Location.Y - 5, txtPassword.Width + 20, txtPassword.Height + 10);
+			using (GraphicsPath path = GetRoundedPath(rectPass, 15))
+			using (var brush = new SolidBrush(Color.FromArgb(240, 242, 245)))
+			{
+				e.Graphics.FillPath(brush, path);
+			}
+		}
 
-        }
-    }
+		private void Header_MouseDown(object sender, MouseEventArgs e)
+		{
+			ReleaseCapture();
+			SendMessage(this.Handle, 0x112, 0xf012, 0);
+		}
+
+		private void ApplyRoundedRegion(Control control, int radius)
+		{
+			Rectangle bounds = new Rectangle(0, 0, control.Width, control.Height);
+			using (GraphicsPath path = GetRoundedPath(bounds, radius))
+			{
+				control.Region = new Region(path);
+			}
+		}
+
+		private GraphicsPath GetRoundedPath(Rectangle rect, int radius)
+		{
+			GraphicsPath path = new GraphicsPath();
+			int d = radius * 2;
+			path.AddArc(rect.X, rect.Y, d, d, 180, 90);
+			path.AddArc(rect.Right - d, rect.Y, d, d, 270, 90);
+			path.AddArc(rect.Right - d, rect.Bottom - d, d, d, 0, 90);
+			path.AddArc(rect.X, rect.Bottom - d, d, d, 90, 90);
+			path.CloseFigure();
+			return path;
+		}
+
+		// --- LOGIKA (Változatlan) ---
+		private async Task<bool> ConnectIfNeeded()
+		{
+			return await _service.ConnectAsync("127.0.0.1", 5000);
+		}
+
+		private async Task HandleLogin()
+		{
+			if (!await ConnectIfNeeded())
+			{
+				MessageBox.Show("Could not connect to server.", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			string user = txtUsername.Text.Trim();
+			string pass = txtPassword.Text.Trim();
+
+			if (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(pass))
+			{
+				await _service.SendLoginAnonAsync();
+			}
+			else
+			{
+				await _service.SendLoginAsync(user, pass);
+			}
+		}
+
+		private async Task HandleRegister()
+		{
+			if (!await ConnectIfNeeded())
+			{
+				MessageBox.Show("Could not connect to server.", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			string user = txtUsername.Text.Trim();
+			string pass = txtPassword.Text.Trim();
+
+			if (!string.IsNullOrEmpty(user) && !string.IsNullOrEmpty(pass))
+			{
+				await _service.SendRegisterAsync(user, pass);
+			}
+			else
+			{
+				MessageBox.Show("Username and password required.", "Registration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+		}
+
+		private void OnMessageReceived(string msg)
+		{
+			if (!IsHandleCreated) return;
+
+			Invoke((MethodInvoker)delegate
+			{
+				if (msg.Contains("Login successful"))
+				{
+					string username = msg.Contains("(Anon)") ? "Anon" : txtUsername.Text;
+					if (string.IsNullOrEmpty(username)) username = "Anon";
+
+					var mainForm = new MainForm(_service, username);
+					mainForm.Show();
+					this.Hide();
+				}
+				else if (msg.Contains("Registration successful"))
+				{
+					MessageBox.Show("Registration successful! You can now login.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+				else if (msg.Contains("failed"))
+				{
+					MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			});
+		}
+	}
 }
