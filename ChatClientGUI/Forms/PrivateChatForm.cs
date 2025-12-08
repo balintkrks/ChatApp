b172ebdb7d6c76925a84acbd4795d6165df3426c
nagynoel79 <nagynoel79@gmail.com>
2025-12-08 12:57:47 +0100

    Consolidated PrivateChatForm logic and UI definitions. Cleaned up duplication to resolve build errors and applied consistent bubble chat styling.

--- ChatClientGUI/Forms/PrivateChatForm.cs
+++ ChatClientGUI/Forms/PrivateChatForm.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ChatClientGUI.Services;
@@ -20,7 +22,48 @@ namespace ChatClientGUI.Forms
 			_service.MessageReceived += OnMessageReceived;
 			btnSendPrivate.Click += async (s, e) => await SendPrivate();
 
+			lstPrivateMessages.MeasureItem += LstPrivateMessages_MeasureItem;
+			lstPrivateMessages.DrawItem += LstPrivateMessages_DrawItem;
+
 			this.FormClosing += (s, e) => _service.MessageReceived -= OnMessageReceived;
+		}
+
+		private void LstPrivateMessages_MeasureItem(object sender, MeasureItemEventArgs e)
+		{
+			e.ItemHeight = 30;
+		}
+
+		private void LstPrivateMessages_DrawItem(object sender, DrawItemEventArgs e)
+		{
+			if (e.Index < 0) return;
+
+			e.DrawBackground();
+			string msg = lstPrivateMessages.Items[e.Index].ToString();
+			Graphics g = e.Graphics;
+			g.SmoothingMode = SmoothingMode.AntiAlias;
+
+			bool isMe = msg.StartsWith("Me:");
+
+			Color bubbleColor = isMe ? Color.DodgerBlue : Color.LightGray;
+			Color textColor = isMe ? Color.White : Color.Black;
+
+			var size = TextRenderer.MeasureText(g, msg, e.Font);
+			int padding = 6;
+			int bubbleWidth = size.Width + 2 * padding;
+			int bubbleHeight = e.Bounds.Height - 6;
+
+			Rectangle bubbleRect;
+			if (isMe)
+				bubbleRect = new Rectangle(e.Bounds.Right - bubbleWidth - 5, e.Bounds.Top + 3, bubbleWidth, bubbleHeight);
+			else
+				bubbleRect = new Rectangle(e.Bounds.Left + 5, e.Bounds.Top + 3, bubbleWidth, bubbleHeight);
+
+			using (var brush = new SolidBrush(bubbleColor))
+				g.FillRectangle(brush, bubbleRect);
+
+			TextRenderer.DrawText(g, msg, e.Font, bubbleRect, textColor, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+
+			e.DrawFocusRectangle();
 		}
 
 		private async Task SendPrivate()
@@ -38,7 +81,6 @@ namespace ChatClientGUI.Forms
 		private void OnMessageReceived(string msg)
 		{
 			if (IsDisposed || !IsHandleCreated) return;
-
 			Invoke((MethodInvoker)delegate
 			{
 				if (msg.StartsWith($"(privát) {_targetUser}:") || msg.StartsWith($"(private) {_targetUser}:"))
